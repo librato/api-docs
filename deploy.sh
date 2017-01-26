45fbb3fbda12d71bf722beb089d161dcbed39547
Greg McKeever <greg@librato.com>
2017-01-26 13:53:48 -0800

    update deploy script to include cname

--- deploy.sh
+++ deploy.sh
@@ -144,12 +144,13 @@ incremental_deploy() {
       return $diff
       ;;
   esac
+  echo "api-docs-archive.librato.com" > CNAME >&2
+  git add CNAME
 }
 
 commit+push() {
   set_user_id
   git --work-tree "$deploy_directory" commit -m "$commit_message"
-
   disable_expanded_output
   #--quiet is important here to avoid outputting the repo URL, which may contain a secret token
   git push --quiet $repo $deploy_branch
